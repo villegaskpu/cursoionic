3fb4e0b9129fdd0824e303e06c12f3853012a015
david <davidvillegas14@gmail.com>
2020-05-07 12:48:24 -0500

    se agrega pagina lista y sliding

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -8,10 +10,26 @@ import { DataService } from '../../services/data.service';
 })
 export class ListPage implements OnInit {
 
+  @ViewChild('lista', { static: false}) lista: IonList;
+
+  usuarios: Observable<any>;
+  
   constructor( private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.getUsers().subscribe(console.log);
+    this.usuarios = this.dataService.getUsers();
   }
 
+  favorite(user) {
+    console.log('favorite', user);
+    this.lista.closeSlidingItems();
+  }
+  share(user) {
+    console.log('share', user);
+    this.lista.closeSlidingItems();
+  }
+  borrar(user) {
+    console.log('borrar', user);
+    this.lista.closeSlidingItems();
+  }
 }
